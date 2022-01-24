359a3276e345ab31fd5fdc1a13892606163e35e4
Nagakushal175 <73735026+Nagakushal175@users.noreply.github.com>
2022-01-24 23:22:15 +0530

    Add files via upload

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,63 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles((theme) => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto Geeks
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat"
+            }}
+          >
+            Those Who Believe In Crypto Also Believe In Cleverness
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
